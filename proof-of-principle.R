0993537cb6d0c2c91fe5289c669de22d4505ec59
jennybc <jenny@stat.ubc.ca>
2016-03-31 11:59:11 -0700

    auth bit for proof of principle experiment

--- proof-of-principle.R
+++ proof-of-principle.R
@@ -1,4 +1,15 @@
-## do google auth, so we have a token on hand to include in httr calls
+scope_list <- c("https://www.googleapis.com/auth/drive",
+                "https://www.googleapis.com/auth/script.storage")
+key <-
+  "178989665258-f4scmimctv2o96isfppehg1qesrpvjro.apps.googleusercontent.com"
+secret <- "iWPrYg0lFHNQblnRrDbypvJL"
+googlestuff_app <- httr::oauth_app("google", key = key, secret = secret)
+google_token <-
+  httr::oauth2.0_token(httr::oauth_endpoints("google"), googlestuff_app,
+                       scope = scope_list, cache = TRUE)
+
+## now drop this into any future httr calls:
+## httr::config(token = google_token)
 
 ## render a local Rmd
 ##   * it has a novel document type in the YAML
