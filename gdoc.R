874dd556852865a78352b9d2091caf0931982433
Noam Ross <noam.ross@gmail.com>
2016-03-31 13:54:27 -0700

    Successful test run of render() function

--- gdoc.R
+++ gdoc.R
@@ -1,11 +1,31 @@
-gdoc <- function(template = "template.docx") {
+gdoc <- function(template = "template.docx", token = google_token, keep_md = FALSE, clean_supporting=TRUE, verbose = TRUE) {
   output_format(
-    post_processor = function(input_file, output_file, clean, verbose) {
+    knitr = knitr_options(),
+    pandoc = pandoc_options(to = "docx"),
+    keep_md = keep_md,
+    clean_supporting = clean_supporting,
+    post_processor = function(metadata, input_file, output_file, clean, verbose) {
+      the_body <- list(title = "test",
+                       mimeType = "application/vnd.google-document")
+      req <- httr::POST("https://www.googleapis.com/drive/v2/files",
+                        httr::config(token = google_token),
+                        body = the_body, encode = "json")
+      rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
+      file_id <- rc$id
+
+      the_url <-
+        file.path("https://www.googleapis.com/upload/drive/v2/files", file_id)
+      the_url <-
+        httr::modify_url(the_url,
+                         query = list(uploadType = "media", convert = TRUE))
+      req <- httr::PUT(the_url,
+                       httr::config(token = google_token),
+                       body = httr::upload_file(output_file))
+      rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
+      rc
+      message(rc$alternateLink)
       # file_id_or_url = upload_to_gdrive (output_file)
       # result  = attach_property(file_id_or_url, readChar(input_file, file.info(input_file)$size)
-
-      message(gdoc_url)
-      message(result)
       return(output_file)
     },
     base_format  = word_document(reference_docx = template)
