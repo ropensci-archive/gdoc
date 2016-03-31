6010c716734cb92844a21e2afc9a810c85dc10d3
jennybc <jenny@stat.ubc.ca>
2016-03-31 11:43:10 -0700

    sketch of an exercise

---
+++ proof-of-principle.R
@@ -0,0 +1,14 @@
+## do google auth, so we have a token on hand to include in httr calls
+
+## render a local Rmd
+##   * it has a novel document type in the YAML
+##   * leaves behind a rendered version
+
+## upload the rendered version to Drive as Google Doc
+## get a Drive fileId back
+
+## via Google Apps script + Execution API
+## attach the Rmd to the rendered Doc as a ?custom document property?
+
+## retrieve the rendered Doc
+## read custom document property = the Rmd source
