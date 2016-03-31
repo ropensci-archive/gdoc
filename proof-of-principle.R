050c518f6c3c6b03c447316c98d924218e4988be
jennybc <jenny@stat.ubc.ca>
2016-03-31 12:21:56 -0700

    add: pushing a word doc up

--- proof-of-principle.R
+++ proof-of-principle.R
@@ -11,12 +11,39 @@ google_token <-
 ## now drop this into any future httr calls:
 ## httr::config(token = google_token)
 
+## JENNY just proving we can now push a Google Doc
+## upload metadata --> get a fileId (Drive-speak)
+local_file <- "test.docx"
+
+the_body <- list(title = "test",
+                 mimeType = "application/vnd.google-document")
+req <- httr::POST("https://www.googleapis.com/drive/v2/files",
+                  httr::config(token = google_token),
+                  body = the_body, encode = "json")
+rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
+(file_id <- rc$id)
+
+## the actual file upload
+the_url <-
+  file.path("https://www.googleapis.com/upload/drive/v2/files", file_id)
+the_url <-
+  httr::modify_url(the_url,
+                   query = list(uploadType = "media", convert = TRUE))
+req <- httr::PUT(the_url,
+                 httr::config(token = google_token),
+                 body = httr::upload_file(local_file))
+rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
+rc
+browseURL(rc$alternateLink)
+
 ## render a local Rmd
 ##   * it has a novel document type in the YAML
 ##   * leaves behind a rendered version
 
 ## upload the rendered version to Drive as Google Doc
 ## get a Drive fileId back
+
+
 
 ## via Google Apps script + Execution API
 ## attach the Rmd to the rendered Doc as a ?custom document property?
