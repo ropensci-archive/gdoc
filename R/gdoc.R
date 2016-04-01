40da0d7d6062d4ff6a60bcb73c9f7fc8ed6a7bbb
Noam Ross <noam.ross@gmail.com>
2016-03-31 17:16:23 -0700

    Move post-processor out of output_format

--- R/gdoc.R
+++ R/gdoc.R
@@ -38,60 +38,65 @@ gtemplate <- function(filename = "test.Rmd") {
 #' @import httr jsonlite rmarkdown
 gdoc <- function(template = NULL, token = gd_auth(),
                  keep_md = FALSE, clean_supporting = TRUE,
-                 verbose = TRUE, browse = TRUE) {
+                 verbose = TRUE, browse = TRUE, upload_Rmd = FALSE) {
+
 
   if (is.null(template)) {
     template = system.file("template.docx", package = "googlestuff")
   }
+
+  post_processor = function(metadata, input_file, output_file, clean, verbose) {
+    if(is.null(metadata$title)) {
+      title = "Test"
+    } else {
+      title = metadata$title
+    }
+    the_body <- list(title = title,
+                     mimeType = "application/vnd.google-apps.document")
+    req <- httr::POST("https://www.googleapis.com/drive/v2/files",
+                      httr::config(token = google_token),
+                      body = the_body, encode = "json")
+    rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
+    file_id <- rc$id
+
+
+    the_url <-
+      file.path("https://www.googleapis.com/upload/drive/v2/files", file_id)
+    the_url <-
+      httr::modify_url(the_url,
+                       query = list(uploadType = "media", convert = TRUE))
+    req <- httr::PUT(the_url,
+                     httr::config(token = google_token),
+                     body = httr::upload_file(output_file))
+    rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
+
+    document_body = rmarkdown:::partition_yaml_front_matter(readLines(input_file))$body
+    front_matter = rmarkdown:::parse_yaml_front_matter(readLines(input_file))
+    front_matter = c(front_matter, gdoc_id = rc$id)
+    front_matter = paste0("---\n",
+                          yaml::as.yaml(front_matter),
+                          "---"
+    )
+
+    cat(front_matter, paste(document_body, collapse="\n"), file=input_file)
+    editURL = file.path("https://docs.google.com/document/d", rc$id, "edit")
+    if(clean_supporting) file.remove(output_file)
+    if(browse) browseURL(editURL)
+    message(editURL)
+    # file_id_or_url = upload_to_gdrive (output_file)
+    # result  = attach_property(file_id_or_url, readChar(input_file, file.info(input_file)$size)
+    return(NULL)
+  }
+
   output_format(
     knitr = knitr_options(),
     pandoc = pandoc_options(to = "docx"),
     keep_md = keep_md,
     clean_supporting = clean_supporting,
-    post_processor = function(metadata, input_file, output_file, clean, verbose) {
-      if (is.null(metadata$title)) {
-        title = "Test"
-      } else {
-        title = metadata$title
-      }
-      the_body <- list(title = title,
-                       mimeType = "application/vnd.google-apps.document")
-      req <- httr::POST("https://www.googleapis.com/drive/v2/files",
-                        httr::config(token = token),
-                        body = the_body, encode = "json")
-      rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
-      file_id <- rc$id
-
-      the_url <-
-        file.path("https://www.googleapis.com/upload/drive/v2/files", file_id)
-      the_url <-
-        httr::modify_url(the_url,
-                         query = list(uploadType = "media", convert = TRUE))
-      req <- httr::PUT(the_url,
-                       httr::config(token = token),
-                       body = httr::upload_file(output_file))
-      rc <- jsonlite::fromJSON(httr::content(req, as = "text", encoding = "UTF-8"))
-
-      editURL <- file.path("https://docs.google.com/document/d", rc$id, "edit")
-      if(browse) browseURL(editURL)
-      message(editURL)
-      # file_id_or_url = upload_to_gdrive (output_file)
-      # result  = attach_property(file_id_or_url, readChar(input_file, file.info(input_file)$size)
-      # save_r_markdown_as_gdoc(input_file, token)
-      return(output_file)
-    },
-    base_format = word_document(reference_docx = template)
+    post_processor = post_processor,
+    base_format  = word_document(reference_docx = template)
   )
 }
 
-save_r_markdown_as_gdoc = function(input_file, token) {
-  script_url = paste0("https://script.googleapis.com/v1/scripts/", .app$script_id, ":run")
-  raw_text = readChar(input_file, file.info(input_file)$size)
 
-  body = list(
-    "function"="saveRMarkdown",
-    parameters=c(file_id, raw_text)
-  )
 
-  invisible(httr::POST(script_url, httr::config(token=token), body=body, encode="json"))
-}
