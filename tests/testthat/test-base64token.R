fa4df88dba8aa69fb2706aa899b31d5270587f60
Noam Ross <noam.ross@gmail.com>
2016-04-04 16:01:07 -0700

    Fix env-to-token test

--- tests/testthat/test-base64token.R
+++ tests/testthat/test-base64token.R
@@ -1,10 +1,10 @@
-TOKEN64 = Sys.getenv("AUTH64")
-
-if(TOKEN64 != "") {
-  base64decode(TOKEN64, output = ".httr-oauth")
-}
 
 test_that("token is present", {
+  TOKEN64 = Sys.getenv("TOKEN64")
+  if(TOKEN64 != "") {
+    token_raw = base64enc::base64decode(what = TOKEN64)
+    writeBin(token_raw, ".httr-oauth")
+  }
   expect_true(file.exists(".httr-oauth"))
 })
 
